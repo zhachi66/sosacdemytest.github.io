d8bfb9f1ccae66e54125f1fa0c860e1c2e587120
zhachi66 <40753131+zhachi66@users.noreply.github.com>
2018-07-05 08:41:35 +0300

    ch

--- event.js
+++ event.js
@@ -1,29 +1,22 @@
 
-$(document).ready(function() {
-  $('#contact-form').submit(function(e) {
-    var name    = document.getElementById('inputName')
-    var email   = document.getElementById('inputEmail')
-    var message = document.getElementById('inputMessage')
-   alert(name.value)
-    if (name.value === "" ) {
-      alertify.error("Please check your entries");
-      alert("Please check your entries")
-      return false;
-    } else {
-      $.ajax({
-        method: 'POST',
-        url: '//formspree.io/zhachi66@gmail.com',
-        data: $('#contact-form').serialize(),
-        datatype: 'json'
-      });
-      e.preventDefault();
-      // $(this).get(0).reset();
-      alertify.success("Message sent");
-    }
-  });
-});
 
 
+
+// $(window).load(function() {
+//    var startSection = window.location.hash;
+//    startSection = "section1";
+
+//     $('html, body').animate({
+//        scrollTop: $(startSection).offset().top
+//      }, 1000, function(){
+
+//        // when done, add hash to url
+//        // (default click behaviour)
+//        window.location.hash = startSection;
+//        consle.log(startSection)
+//      });
+
+// });
 
 
 $("#nav ul li a[href^='#']").on('click', function(e) {
